5b3bff8f6f1d19b742d909b329bb2432d5475a1b
Amandine Jala <amandine.jala@kcl.ac.uk>
2017-01-31 11:17:35 +0000

    Removed Robot object instantiation to keep everything in Test class

--- tests/sensorsTests/GyroPidTest.java
+++ tests/sensorsTests/GyroPidTest.java
@@ -2,33 +2,82 @@ package tests.sensorsTests;
 
 import lejos.hardware.Button;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.hardware.sensor.EV3UltrasonicSensor;
-import main.Robot;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 import java.io.IOException;
 
 public class GyroPidTest {
+
+    private static EV3LargeRegulatedMotor motorL;
+    private static EV3LargeRegulatedMotor motorR;
+    private static EV3GyroSensor gyro;
+
     public static void main(String args[]) throws IOException {
-        EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
-        EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.D);
-        EV3MediumRegulatedMotor visionMotor = new EV3MediumRegulatedMotor(MotorPort.B);
+        motorL = new EV3LargeRegulatedMotor(MotorPort.A);
+        motorR = new EV3LargeRegulatedMotor(MotorPort.D);
 
-        EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
-        EV3GyroSensor gyro = new EV3GyroSensor((Port) SensorPort.S1);
+        gyro = new EV3GyroSensor((Port) SensorPort.S1);
 
-        Robot r = new Robot(motorL, motorR, visionMotor, null, ultrasonicSensor, gyro);
         Button.waitForAnyPress();
 
-        r.start();
-        r.goForward();
+        goForward();
 
-        r.stop();
+        motorL.stop();
+        motorR.stop();
 
-        r.shutdown();
+        motorL.close();
+        motorR.close();
+    }
+
+    private static void goForward() {
+        SampleProvider sampleProvider = gyro.getAngleMode();
+        float[] sample = new float[sampleProvider.sampleSize()];
+        sampleProvider.fetchSample(sample, 0);
+
+        float kp = 10f;
+        float ki = 0f;
+        float kd = 0f;
+        float offset = 0f;
+        int tp = 20;
+        float integral = 0f;
+        float derivative = 0f;
+        float lastError = 0f;
+
+        while (true) {
+            // takes sample
+            sampleProvider.fetchSample(sample, 0);
+
+            float angleVal = sample[0];
+            float error = angleVal - offset;
+            integral += error;
+            derivative = error - lastError;
+
+            adjustSpeed(kp, ki, kd, tp, integral, derivative, error);
+
+            lastError = error;
+
+            Delay.msDelay(5);
+        }
+    }
+
+    private static void adjustSpeed(float kp, float ki, float kd, int tp, float integral, float derivative, float error) {
+        float turn = kp * error + ki * integral + kd * derivative;
+        float powerL = tp + turn;
+        float powerR = tp - turn;
+
+        setSpeed(powerR, powerL);
+
+        motorL.forward();
+        motorR.forward();
+    }
+
+    private static void setSpeed(float powerL, float powerR) {
+        motorL.setSpeed(powerL);
+        motorR.setSpeed(powerR);
     }
 }
