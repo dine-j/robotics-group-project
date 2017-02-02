f294a04c5e43708730a344d97fbe829a7fe19c7d
dine-j <dine.j.neko@gmail.com>
2017-02-02 16:23:09 +0000

    [Error analysis - Path navigation] First commit

---
+++ main/term2Challenges/PathNavigationErrorAnalysis.java
@@ -0,0 +1,69 @@
+package main.term2Challenges;
+
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
+
+public class PathNavigationErrorAnalysis {
+
+    private static RegulatedMotor motorL;
+    private static RegulatedMotor motorR;
+    private static EV3GyroSensor gyro;
+
+    private static final int DEGREES_PER_METER = 2100;
+
+    public static void main(String[] args) {
+        motorL = new EV3LargeRegulatedMotor(MotorPort.D);
+        motorR = new EV3LargeRegulatedMotor(MotorPort.A);
+
+        gyro = new EV3GyroSensor((Port) SensorPort.S1);
+
+        Button.waitForAnyPress();
+        
+        moveForward(DEGREES_PER_METER * 80 / 100);
+        turn(90);
+        moveForward(DEGREES_PER_METER * 50 / 100);
+        turn(-45);
+        moveForward(DEGREES_PER_METER * 50 / 100);
+    }
+
+    /*
+    * Make robot move forward for given distance
+     */
+    private static void moveForward(int distance) {
+        int distance = DEGREES_PER_METER / 50;
+        motorL.rotate(distance, true);
+        motorR.rotate(distance);
+    }
+    
+    /*
+    * Make robot turn by given angle
+     */
+    private static void turn(int angle) {
+        gyro.reset();
+        SampleProvider sampleProvider = gyro.getAngleMode();
+        float[] sample = new float[sampleProvider.sampleSize()];
+        sampleProvider.fetchSample(sample, 0);
+        if(angle > 0) {
+        		while(sample[0] < angle) {
+            	motorR.rotate(10, true);
+            	motorL.rotate(-10);
+            	sampleProvider.fetchSample(sample, 0);
+            	Delay.msDelay(2);
+        		}
+        } else {
+        		while(sample[0] > angle) {
+            	motorR.rotate(10, true);
+            	motorL.rotate(-10);
+            	sampleProvider.fetchSample(sample, 0);
+            	Delay.msDelay(2);
+        		}
+        }
+    }
+}
