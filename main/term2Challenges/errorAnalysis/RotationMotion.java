6b843f16728660ff241ad7f5a0f2d7c46a5c46c7
Amandine Jala <amandine.jala@kcl.ac.uk>
2017-02-07 11:54:23 +0000

    [Rotation error analysis] Rotation in circle

--- main/term2Challenges/errorAnalysis/RotationMotion.java
+++ main/term2Challenges/errorAnalysis/RotationMotion.java
@@ -9,13 +9,10 @@ import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
 
-public class SquareRotation {
-
+public class RotationMotion {
     private static EV3LargeRegulatedMotor motorL;
     private static EV3LargeRegulatedMotor motorR;
     private static EV3GyroSensor gyro;
-
-    private static final int DEGREES_PER_METER = 2100;
 
     public static void main(String[] args) {
         motorL = new EV3LargeRegulatedMotor(MotorPort.D);
@@ -25,53 +22,23 @@ public class SquareRotation {
 
         Button.waitForAnyPress();
 
-        for(int i = 0; i < 4; ++i) {
-            travel();
-            Delay.msDelay(1000);
-            rotatePID();
-            Delay.msDelay(1000);
-        }
-    }
-
-    /*
-    * Make robot travel for 50 cm
-     */
-    private static void travel() {
-        motorL.setSpeed(120);
-        motorR.setSpeed(120);
-        int distance = DEGREES_PER_METER / 2;
-        motorL.rotate(distance, true);
-        motorR.rotate(distance);
-    }
-
-    /*
-    * Make robot rotate 90 degrees anticlockwise
-     */
-    private static void rotate() {
-        gyro.reset();
-        SampleProvider sampleProvider = gyro.getAngleMode();
-        float[] sample = new float[sampleProvider.sampleSize()];
-        sampleProvider.fetchSample(sample, 0);
-
-        motorL.setSpeed(120);
-        motorR.setSpeed(120);
-
-        while(sample[0] < 90) {
-            motorR.rotate(10, true);
-            motorL.rotate(-10);
+        for(int i = 0; i < 20; ++i) {
+            gyro.reset();
+            SampleProvider sampleProvider = gyro.getAngleMode();
+            float[] sample = new float[sampleProvider.sampleSize()];
             sampleProvider.fetchSample(sample, 0);
-//            Delay.msDelay(2);
-        }
 
-        // Debugging
-//        float angle = sample[0];
-//        System.out.println(angle);
+            float angle = sample[0];
+            System.out.println(angle);
+
+            Delay.msDelay(1000);
+        }
     }
 
     /*
     * Make robot rotate 90 degrees anticlockwise using PID
      */
-    private static void rotatePID() {
+    private static void rotatePID(int rotationValue) {
         gyro.reset();
         SampleProvider sampleProvider = gyro.getAngleMode();
         float[] sample = new float[sampleProvider.sampleSize()];
@@ -84,9 +51,9 @@ public class SquareRotation {
         float integral = 0f;
         float derivative = 0f;
 
-        while(sample[0] < 90) {
+        while(sample[0] < rotationValue) {
             float angle = sample[0];
-            float error = angle - 90;
+            float error = angle - rotationValue;
 
             float turn = kp * error + ki * integral + kd * derivative;
             float powerL = tp + turn;
