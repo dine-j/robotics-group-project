b9cb8ebb8473c322b4e3a388fc22ed133332612f
Maksymilian Gajowniczek <maksymilian.gajowniczek@kcl.ac.uk>
2017-03-12 18:40:02 +0000

    Clean up code in GridTest.java

--- tests/aStarTest/GridTest.java
+++ tests/aStarTest/GridTest.java
@@ -5,14 +5,10 @@ import main.term2Challenges.GridGeo;
 
 /**
  * Used to check if all the Grid map-placing methods functioned correctly
- * 
- *
  */
 public class GridTest {
     public static void main(String[] args) throws InterruptedException {
         Grid model = new Grid();
-        //model.findGoalNodeFromRoot(40, 30);
-        
         int size = model.getSize();
         GridFrame grid = new GridFrame(model.getSize());
         
@@ -25,10 +21,17 @@ public class GridTest {
         }
         
         double[] tmp = model.inputTunnelPosition(82.5, 110, 90);
-//        double[] tmp = model.inputTunnelPosition(82.5, 80, 15);
-        model.inputCylinderPosition(40, 122-40);
-        model.inputWallPosition(20, 0, 122, 100, 1);   // 'invisible' wall to reduce search-space
+        model.inputCylinderPosition(GridGeo.RAND_CYCL_31cm_Center);
         model.inputCorners();
+        
+        //further testing
+//        model.inputCylinderPosition(GridGeo.RED_CYCL_31cm_Center);
+        model.inputCylinderPosition(GridGeo.GREEN_CYCL_14cm_Center);
+        
+        //test invisible walls on way back (challenge 2)
+        model.inputWallPosition(0, 120, 60, 60, 4);
+        model.inputWallPosition(0,0 , 60, 60, 4);
+        
         grid.readGrid(model);
         grid.placeGoal(GridGeo.closestNodeInNodeCoords(tmp[1], tmp[0]));
         grid.placeRobot(20,20);
