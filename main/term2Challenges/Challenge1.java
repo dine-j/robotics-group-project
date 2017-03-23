3df7aa1b06f8c0ac63d1075cf41c79a674194ee1
Maksymilian Gajowniczek <maksymilian.gajowniczek@kcl.ac.uk>
2017-03-23 17:47:15 +0000

    Reindent + comment challenge1.java

--- main/term2Challenges/Challenge1.java
+++ main/term2Challenges/Challenge1.java
@@ -15,67 +15,70 @@ import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.utility.Delay;
 
+/**
+ * Localize Robot, follow path to infront of goal, follow path to wall at start location
+ * Motors: left= A right = D
+ * Sensors: Color= 4,lowerTouch=3, upperTouch=2,  Gyro = 1
+ */
 public class Challenge1 {
 
-	public static void main(String[] args) {
-		EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
-		EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.D);
+    public static void main(String[] args) {
+        EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.A);
+        EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.D);
 
-		EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S1);
-		EV3UltrasonicSensor upperTouchSensor = new EV3UltrasonicSensor(SensorPort.S2);
-		EV3TouchSensor bottomTouchSensor = new EV3TouchSensor(SensorPort.S3);
-		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
+        EV3GyroSensor gyroSensor = new EV3GyroSensor(SensorPort.S1);
+        EV3UltrasonicSensor upperTouchSensor = new EV3UltrasonicSensor(SensorPort.S2);
+        EV3TouchSensor bottomTouchSensor = new EV3TouchSensor(SensorPort.S3);
+        EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S4);
 
-		Robot r = new Robot(motorL, motorR, colorSensor, upperTouchSensor, gyroSensor, bottomTouchSensor);
+        Robot r = new Robot(motorL, motorR, colorSensor, upperTouchSensor, gyroSensor, bottomTouchSensor);
 
-		Button.waitForAnyPress();
+        Button.waitForAnyPress();
 
-		// Measure drift 
-		if(r.isSensorDrifting())
-			return;
-		
-		// Localize with Bayesian 'strip'
-		int n = r.localize();  
-		System.out.println(n);
-//		Delay.msDelay(6000); // found goal (hopefully)
+        // Measure drift 
+        if(r.isSensorDrifting())
+            return;
+        
+        // Localize with Bayesian 'strip'
+        int n = r.localize();  
+        System.out.println(n);
+        //int n = 20; // stub - the last white square within two lines
 
-		// Make a sound
-
-		// Goal using A * (doesn't have to go inside)
-		//int n = 20; // stub - the last white square within two lines
-		
-		//Calculate where the robot center was based on where it finished reading.
-		double[] startPosition = GridGeo.actualRobotCenterSW(GridGeo.BayesianCoordinate(n));
-		// startPosition should be about 2 cells behind the colour sensor in current build
-		
-		//debugging - Prints where center of robot was immediately after localization
-		System.out.printf("%.1f , %.1f", startPosition[0], startPosition[1]);
-		
-		double[] firstNodePosition = GridGeo.nextNodeOnLeadingDiagonal(startPosition);
-		// calculate diagonal-distance to move based of x coordinate difference
-		double distToMoveOnDiagonal = (firstNodePosition[0] - startPosition[0]) * RobotMovement.SQRT2;
-		r.moveDistance(distToMoveOnDiagonal); 
-		
-		Grid model = new Grid();
-		double[] goalCoords = model.initClosedList1();
-		Node goalNode = model.aStarSearch(firstNodePosition, goalCoords);
+        
+        //Calculate where the robot center
+        double[] startPosition = GridGeo.actualRobotCenterSW(GridGeo.BayesianCoordinate(n));
+        // startPosition should be about 2 cells behind the colour sensor
+        
+        //debugging - Prints where center of robot was immediately after localization
+        // System.out.printf("%.1f , %.1f", startPosition[0], startPosition[1]);
+        
+        double[] firstNodePosition = GridGeo.nextNodeOnLeadingDiagonal(startPosition);
+        // calculate diagonal-distance to move based of x coordinate difference
+        double distToMoveOnDiagonal = (firstNodePosition[0] - startPosition[0]) * RobotMovement.SQRT2;
+        r.moveDistance(distToMoveOnDiagonal); 
+        
+        // Goal using A * (doesn't have to go inside)
+        Grid model = new Grid();
+        double[] goalCoords = model.initClosedList1();
+        Node goalNode = model.aStarSearch(firstNodePosition, goalCoords);
         LinkedList<Node> list = model.findForwardPath(goalNode);
         List<RobotMovement> actionList = RobotMovement.parsePathToMovements(list);
-		double nodeDiagonal = RobotMovement.SQRT2 * model.getNodeSize();
+        double nodeDiagonal = RobotMovement.SQRT2 * model.getNodeSize();
         r.followInstructions(actionList, model.getNodeSize(), nodeDiagonal);
         
         Delay.msDelay(3000); // found goal (hopefully)
+        //Sound.beep();
         
-		// Going back to starting point
-		System.out.println("Start to go back");
+        // Going back to starting point
+        // System.out.println("Start to go back");
         list = model.findBackwardPath(goalNode);
         actionList = RobotMovement.parsePathToMovements(list);
 
-		// Add extra movement to face wall
-		int wallDirection = RobotMovement.SW;
-		actionList.add(RobotMovement.dirChange(wallDirection));
+        // Add extra movement to face wall
+        int wallDirection = RobotMovement.SW;
+        actionList.add(RobotMovement.dirChange(wallDirection));
 
         r.followInstructions(actionList, model.getNodeSize(), nodeDiagonal);
-		r.moveToWall();
-	}
+        r.moveToWall();
+    }
 }
