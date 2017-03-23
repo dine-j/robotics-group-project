ce4f3495bb303606c8c688d194d3e751d89cd665
Maksymilian Gajowniczek <maksymilian.gajowniczek@kcl.ac.uk>
2017-03-23 18:05:48 +0000

    Reindent GridGeo.java

--- main/term2Challenges/GridGeo.java
+++ main/term2Challenges/GridGeo.java
@@ -7,46 +7,46 @@ package main.term2Challenges;
  * 3) Helping the robot find it's coordinates on the Bayesian strip
  */
 public class GridGeo {
-	
+
     public static final int NODES_PER_EDGE = 62; // 62 means 2cm between each node
-//	public static final int NODES_PER_EDGE = 15; //smaller grid for debugging
+    //public static final int NODES_PER_EDGE = 15; //smaller grid for debugging
     public static final double COURSE_WIDTH = 122; // in cm
     public static final double CORNER_DIAG_WIDTH = 21; // in cm
     public static final double BAYESIAN_ZEROTH_DIAG = CORNER_DIAG_WIDTH + 2; // in cm
     public static final double BAYESIAN_DETECTOR_TO_ROBOT_CENTER_DIST = 4.5; // in cm
     
-    public static final double NODE_SIZE = (double) COURSE_WIDTH / (double) (NODES_PER_EDGE - 1);	
+    public static final double NODE_SIZE = (double) COURSE_WIDTH / (double) (NODES_PER_EDGE - 1);    
     public static final double BAYESIAN_GAP_DIST = 2; //in cm
     public static final double CYLINDER_RADIUS = 2.5 + 3; // in cm
-	public static final double TUNNEL_WALL_RADIUS = 0.9; // in cm
-	public static final double[] TUNNEL_FRONT_LEFT = new double[]{-11,-14}; //new double[]{-11,-10};
-	public static final double[] TUNNEL_FRONT_RIGHT = new double[]{11,-14};//new double[]{11,-10};
-	public static final double[] TUNNEL_BACK_LEFT = new double[]{-11,9};
-	public static final double[] TUNNEL_BACK_RIGHT = new double[]{11,9};
-	
-	/*
-	 *Can add as many cylinder positions as want to test out 
-	 */
-	public static final double[] RAND_CYCL_31cm_Center = new double[]{40, 82};
-	public static final double[] RAND_CYCL_14cm_Center = new double[]{50, 72};
-	public static final double[] GREEN_CYCL_14cm_Center = new double[]{72, 50};
-	public static final double[] RED_CYCL_31cm_Center = new double[]{82, 40};
-	public static final double[] TUNNEL_BeginMarch_Center = new double[]{82.5, 110};
-	
-//	public static final double[] TUNNEL_BeginMarch_Center = new double[]{82.5, 112};
-	
-	public static final double[] CHALLENGE2_BACK_TO_START = new double[]{30, 16};
-	
-	/**
-	 * @return The result of adding offsetVector[] to {x, y}
-	 */
-	public static double[] correctOffset(double x, double y, double[] offsetVector){
-		return new double[]{x + offsetVector[0], y + offsetVector[1]};
-	}
-	
-	/**
-	 * @return The closest node in 'NodeScale' coordinates
-	 */
+    public static final double TUNNEL_WALL_RADIUS = 0.9; // in cm
+    public static final double[] TUNNEL_FRONT_LEFT = new double[]{-11,-14}; //new double[]{-11,-10};
+    public static final double[] TUNNEL_FRONT_RIGHT = new double[]{11,-14};//new double[]{11,-10};
+    public static final double[] TUNNEL_BACK_LEFT = new double[]{-11,9};
+    public static final double[] TUNNEL_BACK_RIGHT = new double[]{11,9};
+    
+    /*
+     *Can add as many cylinder positions as want to test out 
+     */
+    public static final double[] RAND_CYCL_31cm_Center = new double[]{40, 82};
+    public static final double[] RAND_CYCL_14cm_Center = new double[]{50, 72};
+    public static final double[] GREEN_CYCL_14cm_Center = new double[]{72, 50};
+    public static final double[] RED_CYCL_31cm_Center = new double[]{82, 40};
+    public static final double[] TUNNEL_BeginMarch_Center = new double[]{82.5, 110};
+    
+    // public static final double[] TUNNEL_BeginMarch_Center = new double[]{82.5, 112};
+    
+    public static final double[] CHALLENGE2_BACK_TO_START = new double[]{30, 16};
+    
+    /**
+     * @return The result of adding offsetVector[] to {x, y}
+     */
+    public static double[] correctOffset(double x, double y, double[] offsetVector){
+        return new double[]{x + offsetVector[0], y + offsetVector[1]};
+    }
+    
+    /**
+     * @return The closest node in 'NodeScale' coordinates
+     */
     public static int[] closestNodeInNodeCoords(double x, double y) {
         double tmpx = x / NODE_SIZE;
         double tmpy = y / NODE_SIZE;
@@ -58,27 +58,27 @@ public class GridGeo {
     }
     
     /**
-	 * @return The closest node in 'cm' coordinates
-	 */
+     * @return The closest node in 'cm' coordinates
+     */
     public static double[] closestNode(double x, double y) {
         int[] tmp = closestNodeInNodeCoords(x, y);
         return new double[]{ ((double)tmp[0]) / NODE_SIZE , ((double)tmp[1]) / NODE_SIZE};
     }
     
     /**
-   	 * @return The nth Bayesian-cell in 'cm' coordinates
-   	 */
+     * @return The nth Bayesian-cell in 'cm' coordinates
+     */
     public static double[] BayesianCoordinate(int n) {
-    	double distanceOnDiagonal = BAYESIAN_ZEROTH_DIAG + BAYESIAN_GAP_DIST * n;
-    	double xOrY = distanceOnDiagonal / RobotMovement.SQRT2;
-    	return new double[]{ xOrY, xOrY};
+        double distanceOnDiagonal = BAYESIAN_ZEROTH_DIAG + BAYESIAN_GAP_DIST * n;
+        double xOrY = distanceOnDiagonal / RobotMovement.SQRT2;
+        return new double[]{ xOrY, xOrY};
     }
     /**
      * @return The next node on leading diagonal relative to currPos[] in 'cm' coordinates
      */
     public static double[] nextNodeOnLeadingDiagonal( double[] currPos){
-    	double xOrY = (Math.ceil(currPos[0]/ NODE_SIZE)) * NODE_SIZE;
-    	return new double[]{ xOrY, xOrY};
+        double xOrY = (Math.ceil(currPos[0]/ NODE_SIZE)) * NODE_SIZE;
+        return new double[]{ xOrY, xOrY};
     }
     
     /**
@@ -86,11 +86,11 @@ public class GridGeo {
      * @return The center of the robot in 'cm' coordinates, if center is SW behind colour sensor
      */
     public static double[] actualRobotCenterSW(double[] colourReaderCenter){
-    	double x = colourReaderCenter[0];
-    	double y = colourReaderCenter[1];
-    	x -= BAYESIAN_DETECTOR_TO_ROBOT_CENTER_DIST / RobotMovement.SQRT2;
-    	y -= BAYESIAN_DETECTOR_TO_ROBOT_CENTER_DIST / RobotMovement.SQRT2;
-    	return new double[]{x,y};
+        double x = colourReaderCenter[0];
+        double y = colourReaderCenter[1];
+        x -= BAYESIAN_DETECTOR_TO_ROBOT_CENTER_DIST / RobotMovement.SQRT2;
+        y -= BAYESIAN_DETECTOR_TO_ROBOT_CENTER_DIST / RobotMovement.SQRT2;
+        return new double[]{x,y};
     }
-	
+    
 }
